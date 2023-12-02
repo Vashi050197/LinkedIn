e0b1652f753ba5bc7f882b02a6e9fb6c9a0e5485
Vashisth <vashi5684@gmail.com>
2023-12-02 17:27:23 +0530

    Add Job Profile of ur choice

--- configTemp.py
+++ configTemp.py
@@ -2,4 +2,5 @@
 # Placeholder values, replace these with your actual credentials
 ACCOUNT_EMAIL = "YOUR EMAIL ID"
 ACCOUNT_PASSWORD = "YOUR PASSWORD"
+KEYWORDS = "JOB PROFILE"        # Python Devloper, Software Engg
 PHONE_NUMBER = "YOUR MOBILE NO"
