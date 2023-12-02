2fe5412f873f30c8e27c3841f56bcb17f7faa46e
Vashisth <vashi5684@gmail.com>
2023-12-02 15:19:10 +0530

    jupyter

---
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,37 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "2cd65646-ac74-4b74-8539-b397cf9087e3",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "ACCOUNT_EMAIL = \"vashi5684@gmail.com\"\n",
+    "ACCOUNT_PASSWORD = \"Ds?nf2=&!iC.qJ)\"\n",
+    "PHONE_NUMBER = \"8077975459\""
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
